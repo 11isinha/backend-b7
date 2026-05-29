2618907385f895c0bc3e904ab4ceb7f6f9921180
isinha🫧 <isabella.r.rosa@aluno.senai.br>
2026-05-29 09:07:36 -0300

    Add files via upload

---
+++ routes/auth.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import { supabase } from '../supabaseClient.js'
+
+const router = express.Router()
+
+// cadastro
+router.post('/register', async (req, res) => {
+  const { email, password } = req.body
+
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password
+  })
+
+  if (error) return res.status(400).json(error)
+
+  res.json(data)
+})
+
+// login
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body
+
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password
+  })
+
+  if (error) return res.status(400).json(error)
+
+  res.json(data)
+})
+
+export default router
---
+++ server.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+import authRoutes from './routes/auth.js'
+import productRoutes from './routes/products.js'
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('API rodando 🚀')
+})
+
+app.use('/auth', authRoutes)
+app.use('/products', productRoutes)
+
+
+
+// 👇 IMPORTANTE PRA VERCEL
+export default app
+
+// 👇 só roda local
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 3000
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`)
+  })
+}
